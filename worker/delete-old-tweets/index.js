96d481d22c8f4cb164f93a158ceed48117b7b117
visualglitch91 <visualglitch91@protonmail.com>
2019-12-30 11:22:44 -0300

    Update index.js

--- worker/delete-old-tweets/index.js
+++ worker/delete-old-tweets/index.js
@@ -21,7 +21,7 @@ async function main() {
         collection.find({
           createdAt: {
             $lte: moment()
-              .subtract(2, 'weeks')
+              .subtract(1, 'day')
               .toDate()
           }
         })
